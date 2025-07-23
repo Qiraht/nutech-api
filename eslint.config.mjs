06791317c2f27e0dc0ddb9c1620c243330b4e7b4
Thariq <thariq2712@gmail.com>
2025-07-23 13:13:03 +0700

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+// prettier requirement
+const prettier = {
+  config: eslintConfigPrettier,
+  plugin: eslintPluginPrettier
+}
+
+export default defineConfig([
+  prettier.config,
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, prettier: prettier.plugin }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
