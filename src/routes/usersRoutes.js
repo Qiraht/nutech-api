a76a4cd2fc43d8af680e601e8681cec49a5dcc0d
Thariq <thariq2712@gmail.com>
2025-07-24 14:32:35 +0700

    add users routes

---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+// post /Registration
+router.post('/registration', async (req, res) => {});
+
+// post /login
+router.post('/login', async (req, res) => {});
+
+// get /Profile
+router.get('/profile', async (req, res) => {});
+
+// put /Profile/Update\
+router.put('/profile/update', async (req, res) => {});
+
+// put /Profile/image
+router.put('/profile/update', async (req, router) => {});
+
+module.exports = router;
