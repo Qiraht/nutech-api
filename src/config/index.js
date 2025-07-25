5a11d58fbe060f84c0483e098d49e072cdb6e802
Thariq <thariq2712@gmail.com>
2025-07-26 00:44:19 +0700

    add db psql config

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,13 @@ const config = {
     host: process.env.HOST,
     port: process.env.PORT,
   },
+  psql: {
+    host: process.env.PGHOST || 'localhost',
+    port: process.env.PGPORT || 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
 };
 
 module.exports = config;
