ae3eb0785f84748962eb71614f1d37a8e2963c9c
Thariq <thariq2712@gmail.com>
2025-07-26 00:50:34 +0700

    initiate container for DI and fix format

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const ErrorHandler = require('./middlwares/ErrorHandler');
+const router = require('./routes');
+const { scopePerRequest } = require('awilix-express');
+const container = require('./container');
 
 const app = express();
 
+const routes = router;
+
 const corsOption = {
-    origin: '*',
-    optionsSuccessStatus: 200,
+  origin: '*',
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOption));
 app.use(express.json());
+app.use(scopePerRequest(container));
+
+// Routes
+app.use(routes);
 
 // Middleware
 app.use(ErrorHandler);
 
 // Health Check
 app.get('/', (req, res) => {
-  res.json({message: 'testing'})
+  res.json({ message: 'Hello World!' });
 });
 
 module.exports = app;
