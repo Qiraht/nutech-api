728d1c6ff4d4d2b6f72d4fa73f20976164cbc228
Thariq <thariq2712@gmail.com>
2025-07-26 00:48:28 +0700

    add register routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,20 +1,37 @@
-const express = require("express");
+const express = require('express');
+const container = require('../container');
+const validateRequestBody = require('../middlwares/ValidateRequestBody');
+const RegisterPayloadSchema = require('../validator/usersSchema');
 
 const router = express.Router();
 
+const userController = container.resolve('userController');
+
 // post /Registration
-router.post('/registration', async (req, res) => {});
+router.post(
+  '/registration',
+  validateRequestBody(RegisterPayloadSchema),
+  userController.postRegistrationController,
+);
 
 // post /login
-router.post('/login', async (req, res) => {});
+router.post('/login', async (req, res) => {
+  res.json({ message: 'Still In Progress!' });
+});
 
 // get /Profile
-router.get('/profile', async (req, res) => {});
+router.get('/profile', async (req, res) => {
+  res.json({ message: 'Still In Progress!' });
+});
 
-// put /Profile/Update\
-router.put('/profile/update', async (req, res) => {});
+// put /Profile/Update
+router.put('/profile/update', async (req, res) => {
+  res.json({ message: 'Still In Progress!' });
+});
 
 // put /Profile/image
-router.put('/profile/update', async (req, router) => {});
+router.put('/profile/update', async (req, res) => {
+  res.json({ message: 'Still In Progress!' });
+});
 
 module.exports = router;
