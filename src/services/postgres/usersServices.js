40181cadd1e2fcbddeecd07c3b256943bff396a1
Thariq <thariq2712@gmail.com>
2025-07-26 00:47:41 +0700

    add register user

--- src/services/postgres/usersServices.js
+++ src/services/postgres/usersServices.js
@@ -1,19 +1,38 @@
-const { Pool } = require("pg");
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
 
-class UsersService {
-    constructor() {
-        this._pool =  new Pool();
-    }
+class UsersServices {
+  constructor({ pool }) {
+    this._pool = pool;
+  }
 
-    async addUsers() {};
+  async registerUser({ email, first_name, last_name, password }) {
+    // create id using nanoid
+    const id = `user-${nanoid(16)}`;
 
-    async addLogin() {};
+    // add date using js date function, timestamptz support iso string
+    const createdAt = new Date().toISOString();
 
-    async getProfile() {};
+    // hashing password for security with bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    async updateProfile() {};
+    const query = {
+      text: `INSERT INTO users (id, email, password, first_name, last_name, created_at)
+            VALUES ($1, $2, $3, $4, $5, $6) 
+            RETURNING id`,
+      values: [id, email, hashedPassword, first_name, last_name, createdAt],
+    };
 
-    async updateProfileImage() {};
-};
+    await this._pool.query(query);
+  }
 
-module.exports = UsersService;
+  async addLogin() {}
+
+  async getProfile() {}
+
+  async updateProfile() {}
+
+  async updateProfileImage() {}
+}
+
+module.exports = UsersServices;
