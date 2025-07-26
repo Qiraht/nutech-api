cbd08701a14d2b2816dae6707153a9d562f680b2
Thariq <thariq2712@gmail.com>
2025-07-27 00:39:41 +0700

    add addUserBalance on transaction service

--- src/services/postgres/transactionService.js
+++ src/services/postgres/transactionService.js
@@ -14,14 +14,26 @@ class TransactionsServices {
         return result.rows[0];
     }
 
-    async addUserBalance(email) {}
+    async addUserBalance(email, amount) {
+        const query = {
+            text: `UPDATE users
+            SET balance = balance + $1
+            WHERE email = $2
+            RETURNING balance`,
+            value: [amount, email],
+        };
+
+        const result = await this._pool.query(query);
+
+        return result.rows[0];
+    }
 
     /*
     ** 1. Check Transaction Code,
     ** 2. get UserId,
     ** 3.
     */
-    async createTransaction({userId, transactionCode, totalAmount}) {
+    async createTransaction({userId, transactionCode}) {
     }
 }
 
