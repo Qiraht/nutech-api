d1d43425382e9b79896f79d6be3a74f16da182f9
Thariq <thariq2712@gmail.com>
2025-07-26 11:47:14 +0700

    fix authentication error

--- src/exceptions/AuthenticationError.js
+++ src/exceptions/AuthenticationError.js
@@ -2,8 +2,8 @@ const ClientError = require('./ClientError');
 
 class AuthenticationError extends ClientError {
   constructor(message) {
-    super(102, message);
-    this.name = 'Authentication Error';
+    super(102, message, 401);
+    this.name = 'AuthenticationError';
   }
 }
 
