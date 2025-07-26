27b39fc5ed6e1846ae7fb5fbb095030a1cd5f786
Thariq <thariq2712@gmail.com>
2025-07-27 00:40:12 +0700

    add transaction controller

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,8 +1,10 @@
 const autoBind = require("auto-bind");
 
 class TransactionsController {
-    constructor({ transacstionService }) {
-        this._transactionService = transacstionService;
+    constructor({ transactionService, userService, informationService }) {
+        this._service = transactionService;
+        this._userService = userService;
+        this._informationService = informationService;
 
         autoBind(this);
     }
@@ -10,7 +12,7 @@ class TransactionsController {
     async getBalanceController(req, res) {
         const user = req.user.payload;
 
-        const balance = await this._transactionService.getUserBalance(user);
+        const balance = await this._service.getUserBalance(user);
 
         res.status(200).json({
             status: 0,
@@ -19,7 +21,19 @@ class TransactionsController {
         });
     };
 
+    async postTopUpController(req, res) {
+        const {user: email } = req.user.payload;
 
+        const { top_up_amount: amount } = req.body;
+
+        const topup = await this._service.addUserBalance(email, amount);
+
+        res.status(200).json({
+          status: 0,
+          message: 'Top Up Balance berhasil',
+          data: topup
+        });
+    }
 }
 
 module.exports = TransactionsController;
