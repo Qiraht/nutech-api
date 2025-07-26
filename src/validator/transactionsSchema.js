ea581cb294bce4937e3295875e864820ce5af41b
Thariq <thariq2712@gmail.com>
2025-07-26 21:44:47 +0700

    add top up schema validation

---
+++ src/validator/transactionsSchema.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+const TopUpPayloadSchema = Joi.object({
+  top_up_amount: Joi.number().greater(0).messages({
+    'number.base':
+      'Parameter top_up_amount hanya boleh angka dan tidak boleh lebih kecil dari 0',
+    'any.required':
+      'Parameter top_up_amount hanya boleh angka dan tidak boleh lebih kecil dari 0',
+    'number.greater':
+      'Paramter amount hanya boleh angka dan tidak boleh lebih kecil dari 0',
+  }),
+});
+
+module.exports = { TopUpPayloadSchema };
