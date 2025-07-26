5cdf4a723d7477bdc140c38d186f39da64fc6f8e
Thariq <thariq2712@gmail.com>
2025-07-27 00:40:56 +0700

    register get balance and post topup controller on routes

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const container = require('../container');
+const authenticationMiddleware = require('../middlwares/AuthenticationMiddleware');
+const validateRequestBody = require('../middlwares/ValidateRequestBody');
+const { TopUpPayloadSchema } = require('../validator/transactionsSchema');
 
 const router = express.Router();
 
+const transactionController = container.resolve('transactionController');
+
 // get /balance
-router.get('/balance', async (req, res) => {
-    res.json({message: 'WIP!'});
-});
+router.get(
+  '/balance',
+  authenticationMiddleware,
+  transactionController.getBalanceController,
+);
 
 // post /topup
-router.post('/topup', async (req, res) => {
-  res.json({ message: 'WIP!' });
-});
+router.post('/topup', validateRequestBody(TopUpPayloadSchema), authenticationMiddleware, transactionController.postTopUpController);
 
 // post /transatcion
 router.post('/transaction', async (req, res) => {
