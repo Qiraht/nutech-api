246e13d9f2a55639f5e96ed1bad5a56c93bae4f7
Thariq <thariq2712@gmail.com>
2025-07-28 23:14:03 +0700

    add map to model for banner and service

---
+++ src/utils/mapServicesToModel.js
@@ -0,0 +1,8 @@
+const MapServicesToModel = ({ code, name, icon, tariff }) => ({
+  service_code: code,
+  service_name: name,
+  service_icon: icon,
+  service_tariff: tariff,
+});
+
+module.exports = MapServicesToModel;
