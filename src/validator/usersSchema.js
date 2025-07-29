494eb3738f52af1a6c89db36c7732af986571ba2
Thariq <thariq2712@gmail.com>
2025-07-29 23:33:12 +0700

    fix validation messages

--- src/validator/usersSchema.js
+++ src/validator/usersSchema.js
@@ -3,45 +3,45 @@ const Joi = require('joi');
 const RegisterPayloadSchema = Joi.object({
   email: Joi.string().email({ tlds: true }).required().messages({
     'string.email': 'Format email tidak sesuai',
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'Email wajib diisi',
+    'string.empty': 'Parameter email harus di isi',
+    'any.required': 'Parameter email harus di isi',
   }),
   first_name: Joi.string().required().messages({
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'first name harus diisi',
+    'string.empty': 'Parameter first_name harus di isi',
+    'any.required': 'Parameter first_name harus di isi',
   }),
   last_name: Joi.string().required().messages({
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'last name harus diisi',
+    'string.empty': 'Parameter last_name harus di isi',
+    'any.required': 'Parameter last_name harus di isi',
   }),
   password: Joi.string().min(8).required().messages({
-    'string.empty': 'Silahkan isi Password',
-    'string.min': 'Password kurang dari 8 karakter',
-    'any.required': 'Password Diperluka',
+    'string.empty': 'Parameter password harus di is',
+    'string.min': 'Password length minimal 8 karakter',
+    'any.required': 'Parameter password harus di is',
   }),
 });
 
 const LoginPayloadSchema = Joi.object({
   email: Joi.string().email({ tlds: true }).required().messages({
     'string.email': 'Format email tidak sesuai',
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'Email wajib diisi',
+    'string.empty': 'Parameter email harus di isi',
+    'any.required': 'Parameter email harus di isi',
   }),
   password: Joi.string().min(8).required().messages({
-    'string.empty': 'Silahkan isi Password',
-    'string.min': 'Password kurang dari 8 karakter',
-    'any.required': 'Password Diperlukan',
+    'string.empty': 'Parameter password harus di is',
+    'string.min': 'Password length minimal 8 karakter',
+    'any.required': 'Parameter password harus di is',
   }),
 });
 
 const EditProfilePayloadSchema = Joi.object({
   first_name: Joi.string().required().messages({
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'first name harus diisi',
+    'string.empty': 'Parameter first_name harus di isi',
+    'any.required': 'Parameter first_name harus di isi',
   }),
   last_name: Joi.string().required().required({
-    'string.empty': 'Silahkan isi Email',
-    'any.required': 'last name harus diisi',
+    'string.empty': 'Parameter last_name harus di isi',
+    'any.required': 'Parameter last_name harus di isi',
   }),
 });
 
